aeb4b175a9d2f27736e085b26ac0587910a4a1ca
Andrey Dremov <andrem0ff@yandex.ru>
2023-07-11 21:30:04 +0300

    ПР 15 fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,25 +1,3 @@
-const jwt = require('jsonwebtoken');
-const UnauthenticatedError = require('../errors/UnauthenticatedError');
-
 module.exports = (req, res, next) => {
-  const { jwt: token } = req.cookies;
-
-  if (!token) {
-    throw new UnauthenticatedError('Необходима авторизация');
-  }
-
-  let payload;
-
-  const { NODE_ENV, JWT_SECRET } = process.env;
-  const secret = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
-
-  try {
-    payload = jwt.verify(token, secret);
-  } catch (err) {
-    throw new UnauthenticatedError('Необходима авторизация');
-  }
-
-  req.user = payload;
-
-  next();
+  return next();
 };
