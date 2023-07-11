c89f316706d4736a515c89ce2709d0799804d8da
Andrey Dremov <andrem0ff@yandex.ru>
2023-07-11 22:14:02 +0300

    ПР 15 fix

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -26,7 +26,7 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, cards, onCardClick, onC
 
       <section className="elements">
         <ul className='elements__list'>
-          {cards.map((card) => (
+          {currentUser._id && cards.map((card) => (
             <Card
               key={card._id}
               card={card}
